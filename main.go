7452250862c13a11f06d1c334e9d0f574fa2cade
Mukul <mukulmanav0@gmail.com>
2022-12-09 20:03:31 +0530

    commited on 9th dec

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
 	"fmt"
 	"vexample/vetest"
@@ -9,9 +10,23 @@ import (
 	"vexample/vmap"
 	"vexample/vstruct"
 )
+*/
+import (
+	"log"
+	"vexample/vcrud"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
+}
 
 func main() {
-	vetest.Veteest()
+	/*vetest.Veteest()
 	vilse.Vilse()
 	vilse.Sc()
 	vfunc.F1()
@@ -24,5 +39,18 @@ func main() {
 	vstruct.Main3()
 	vstruct.Main4()
 	vint.Vin2()
+	*/
 
+	//GORM COMMANDS
+
+	vcrud.Init()
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
